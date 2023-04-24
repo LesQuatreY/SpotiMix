493000d9a0b7f2cd238db5798074cf5a171126cb
LesQuatreY <tanguy.minot@laposte.net>
2023-04-24 16:31:43 +0200

    clean

--- app.py
+++ app.py
@@ -1,31 +1,57 @@
-import pandas as pd
+import random
 import streamlit as st
 import spotify_id_access
 
 from sklearn.cluster import KMeans
 from spotify_playlist_fetcher import SpotifyPlaylistFetcher
 
-tracker = SpotifyPlaylistFetcher(spotify_id_access.client_id, spotify_id_access.client_secret)
+# """
+# Demande à l'utilisateur le lien de la playlist
+# """
+playlist_url = st.text_input("Insérer le lien de votre playlist Spotify :")
+# https://open.spotify.com/playlist/1g5V0tiecUw0zuaKlIsF24?si=9bc6da25690a4ff1
+if not playlist_url: st.stop()
 
+# """
+# Demande à l'utilisateur le seuil de la longueur de la nouvelle playlist
+# """
+playlist_length_threshold = st.number_input("Entrez la longueur minimale de la nouvelle playlist :", min_value=1, value=10, step=1)
+
+# """
+# Récupération des informations des musiques de la playlist
+# """
+tracker = SpotifyPlaylistFetcher(spotify_id_access.client_id, spotify_id_access.client_secret)
 data_for_clustering = [
      [track[key] for key in ['acousticness', 'danceability', 'energy', 'instrumentalness', 'loudness', 'speechiness', 'tempo']]
-for track in tracker.get_playlist_tracks("https://open.spotify.com/playlist/1g5V0tiecUw0zuaKlIsF24?si=9bc6da25690a4ff1")
+for track in tracker.get_playlist_tracks(playlist_url)
 ]
 
+# """
+# Classification avec Kmeans
+# """
 nb_clusters = int(len(data_for_clustering)/2)
 kmeans = KMeans(n_clusters=nb_clusters, max_iter=1000).fit(data_for_clustering)
 
-to_add = tracker.searchNew("rap")
-newPlaylist = []
+values = list(tracker.get_playlist_genres(playlist_url).values())
 
-for elem in to_add:
-    to_pred = [[elem["acousticness"], elem["danceability"], elem["energy"],elem["instrumentalness"], elem["loudness"], elem["speechiness"], elem["tempo"]]]
-    kmeans.predict(to_pred)
-    score = abs(kmeans.score(to_pred))
-    if score < 0.90 and elem["id"] not in newPlaylist:
-        newPlaylist.append(elem["id"])
-        print("score for "+elem["id"]+" : ", score,"| Len new playlist : ", len(newPlaylist))
-    
-    if len(newPlaylist) >= 35:
-        print("quit")
-        break
+newPlaylist = []
+while len(newPlaylist) < playlist_length_threshold:
+    genre = random.choices(
+        list(tracker.get_playlist_genres(playlist_url).keys()), 
+        weights=[value / sum(values) for value in values]
+        )[0]
+    for elem in tracker.searchNew(genre):
+        to_pred = [[elem["acousticness"], elem["danceability"], elem["energy"],elem["instrumentalness"], elem["loudness"], elem["speechiness"], elem["tempo"]]]
+        kmeans.predict(to_pred)
+        score = abs(kmeans.score(to_pred))
+        if score < 0.90 and elem["id"] not in newPlaylist:
+            newPlaylist.append(elem["id"])
+            print("score for "+elem["id"]+" : ", score,"| Len new playlist : ", len(newPlaylist))
+        
+        if len(newPlaylist) >= 35:
+            print("quit")
+            break
+    st.progress(len(newPlaylist), "En progression...")
+
+for music in newPlaylist:
+    st.write("https://open.spotify.com/track/" + music)
--- spotify_playlist_fetcher.py
+++ spotify_playlist_fetcher.py
@@ -1,9 +1,7 @@
 import random
 import string
 import spotipy
-import spotify_id_access
 
-from sklearn.cluster import KMeans
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
@@ -13,41 +11,18 @@ class SpotifyPlaylistFetcher:
             self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     
     def get_playlist_tracks(self, playlist_link):
-        return [self.sp.audio_features(track["track"]["uri"])[0] for track in self.sp.playlist_tracks(playlist_link.split("/")[-1].split("?")[0])["items"]] 
-    
+        return [self.sp.audio_features(track["track"]["uri"])[0] for track in self.sp.playlist_tracks(playlist_link.split("/")[-1].split("?")[0])["items"]]
+        
     def get_music_tracks(self, music_link):
          return self.sp.audio_features(music_link.split("/")[-1])[0]
     
+    def get_playlist_genres(self, playlist_link):
+         return {genre:sum(1 for track in self.sp.playlist_tracks(playlist_link)['items'] for artist in track['track']['artists'] if genre in self.sp.artist(artist['id'])['genres']) for genre in set(genre for track in self.sp.playlist_tracks(playlist_link)['items'] for artist in track['track']['artists'] for genre in self.sp.artist(artist['id'])['genres'])}
+
     def searchNew(self, genre):
           random_char = random.choice(string.ascii_letters)
           query = f'track:"{random_char}" genre:"{genre}"'
           results = self.sp.search(q=query, type="track", limit=50)
-
           ids_song_to_add = [track["id"] for track in results["tracks"]["items"]]
           audio_features = self.sp.audio_features(tracks=ids_song_to_add)
           return audio_features 
-
-tracker = SpotifyPlaylistFetcher(spotify_id_access.client_id, spotify_id_access.client_secret)
-
-data_for_clustering = [
-     [track[key] for key in ['acousticness', 'danceability', 'energy', 'instrumentalness', 'loudness', 'speechiness', 'tempo']]
-for track in tracker.get_playlist_tracks("https://open.spotify.com/playlist/1g5V0tiecUw0zuaKlIsF24?si=9bc6da25690a4ff1")
-]
-
-nb_clusters = int(len(data_for_clustering)/2)
-kmeans = KMeans(n_clusters=nb_clusters, max_iter=1000).fit(data_for_clustering)
-
-to_add = tracker.searchNew("rap")
-newPlaylist = []
-
-for elem in to_add:
-    to_pred = [[elem["acousticness"], elem["danceability"], elem["energy"],elem["instrumentalness"], elem["loudness"], elem["speechiness"], elem["tempo"]]]
-    kmeans.predict(to_pred)
-    score = abs(kmeans.score(to_pred))
-    if score < 0.90 and elem["id"] not in newPlaylist:
-        newPlaylist.append(elem["id"])
-        print("score for "+elem["id"]+" : ", score,"| Len new playlist : ", len(newPlaylist))
-    
-    if len(newPlaylist) >= 35:
-        print("quit")
-        break
